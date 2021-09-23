b9f35277b752ac846fcd70e3350f7676499bc28f
fbianx <al221911909@gmail.com>
2021-09-23 13:00:33 -0500

    Layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.index')
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <h1>
+            Mes Interface It
+            </h1>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar fixed-top navbar-dark bg-dark">
+  <!-- Navbar content -->
+  <div class="container">
+  
+    <a class="navbar-brand" href="#">Mes interface It</a>
+  
+    </div>
+</nav>
