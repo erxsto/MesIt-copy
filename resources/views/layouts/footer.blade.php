1861b5dfa4c8181c3133b65ba1edc1fec3b94826
fbianx <al221911909@gmail.com>
2021-09-30 11:38:22 -0500

    iconos

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,36 @@
 <center>
-    <hr>
-<h5 class="copyright">© Copyright AMATS ELECTRIC S.A. de C.V. 2021</h5>
+<div id="copyright text-right" class="alert alert-primary">© Copyright AMATS ELECTRIC S.A. de C.V. 2021 <br><br>
+Empresa 100% mexicana, distribuye, vende, da soporte técnico, asesora, y capacita a la industria de la automatización.
 
-</center>
+
+    <br><br>
+    <p> REDES SOCIALES <br>
+    Compartimos las soluciones más novedosas en automatización y control de movimiento, ¡Síguenos! <br>
+    </p>
+    <a href="https://www.facebook.com/amatselectric" target="_blank" class="hoverit fa fa-facebook mt-4 alert-primary "></a>
+    <a href="https://mx.linkedin.com/company/amatselectric" target="_blank" class="hoverit fa fa-linkedin alert-primary" style="margin-left:4px;"></a>
+    <a href="https://www.twitter.com/amatselectric" target="_blank" class="hoverit fa fa-twitter alert-primary"  style="margin-left:4px;"></a>
+    <a href="https://www.youtube.com/channel/UCAgEBjo3r2FdedNkefuDGhA" target="_blank" class="hoverit fa fa-youtube alert-primary"  style="margin-left:4px;"></a>
+    </center> 
+</div>
+
+<!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "7227749519", // WhatsApp number
+            call_to_action: "Envíanos un mensaje", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+
+
+<!-- /GetButton.io widget -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <script>
 const checkbox = 
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,21 +1,26 @@
+
 <nav class="menu navbar navbar-expand-lg navbar-custom">
   <!-- Navbar content -->
-  <img style="width:150px; height:100;"  src="https://th.bing.com/th/id/R.5e75ad560b549806add5565398d807ad?rik=u91Wyo0r%2bu3TwA&riu=http%3a%2f%2fwww.amats.com.mx%2fimg%2famats-electric-logo.png&ehk=KxO4DkCYqqOrBWYPOtjKTDHaXA9i3W2ieOG7Fekeo0o%3d&risl=&pid=ImgRaw&r=0" alt="amats_logo">
+  <img style="width:150px; height:100;"  src="http://amats.com.mx/images/logo-amats-electric-r.svg" alt="amats_logo">
   <div class="container">
-    <div class="navbar-brand navbar-light" href="#">MES INTERFACE IT</div>
+    <div style="#CEECF5" class="navbar-brand navbar-light" href="#">MES INTERFACE IT</div>
     
     <ul class="navbar-nav">
       
-      <li class="nav-item active">
-        <a class="nav-link" href="./vibracion">Gráfica Vibración </a>  
+     <li class="nav-item active">
+        <a class="hoverit nav-link btn-lg" href="./"><i class="bi bi-house" ></i> Dashboard</a>  
       </li>
 
-      <li class="nav-item active">
-        <a class="nav-link" href="./energia">Energía </a>
+      <li class="nav-item active" style="margin-left:5px;">
+        <a class="hoverit nav-link btn-lg" href="./vibracion"><i class="bi bi-bezier2"></i>Gráfica Vibración </a>  
       </li>
 
-      <li class="nav-item active">
-        <a class="nav-link" href="./temperatura">Temperatura </a>
+      <li class="nav-item active" style="margin-left:5px;">
+        <a class="hoverit nav-link btn-lg" href="./energia"><i class="fa fa-car-battery"></i> Energía </a>
+      </li>
+
+      <li class="nav-item active" style="margin-left:5px;">
+        <a class="hoverit nav-link btn-lg" href="./temperatura"><i class="bi bi-speedometer2"></i> Temperatura </a>
       </li>
       <input type="checkbox" class="checkbox" id="checkbox">
       <label for="checkbox" class="label">
@@ -26,3 +31,4 @@
     </ul>
     </div>
 </nav>
+    
