796edb8350a41be74cb180d27ec5c530ccb6c5df
erxsto <al221911907@gmail.com>
2021-10-11 12:35:40 -0500

    filtrado por fecha

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,5 @@ use App\Http\Controllers\VibracionController;
 
 Route::name('/')->get('/', 'App\Http\Controllers\DashboardController@index');
 Route::name('vibracion')->get('vibracion/', 'App\Http\Controllers\VibracionController@index');
-Route::name('fecha')->get('fecha/', 'App\Http\Controllers\VibracionController@fecha');
 Route::name('temperatura')->get('temperatura/', 'App\Http\Controllers\TemperaturaController@index');
 Route::name('energia')->get('energia/', 'App\Http\Controllers\EnergiaController@index');
