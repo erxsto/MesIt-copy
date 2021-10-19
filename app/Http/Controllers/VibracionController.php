98b094ae4d1a5bfb0721b04dd7930dcf35a9843c
erxsto <al221911907@gmail.com>
2021-10-19 09:09:36 -0500

    whatsap en 3 ejes

--- app/Http/Controllers/VibracionController.php
+++ app/Http/Controllers/VibracionController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Twilio\Rest\Client;
 
 use Illuminate\Http\Request;
@@ -11,12 +12,12 @@ class VibracionController extends Controller
 {
     public function index(Request $request)
     {
-        $fi = $request->fecha_ini.' 00:00:00';
-        $ff = $request->fecha_fin.' 23:59:59';
+        $fi = $request->fecha_ini . ' 00:00:00';
+        $ff = $request->fecha_fin . ' 23:59:59';
         $graficas = grafica_ejes::whereBetween('created_at', [$fi, $ff])->get();
 
         return view('content.vibracion')
-        ->with(['graficas' => $graficas]);
+            ->with(['graficas' => $graficas]);
     }
     public function datavibracion()
     {
@@ -33,46 +34,83 @@ class VibracionController extends Controller
         $graficas = grafica_ejes::latest()->take(1)->get()->sortBy('id');
         $labels = $graficas->pluck('id');
         $data = $graficas->pluck('ejez');
+
+        if ($data[0] >= 2.9) {
+
+            require_once '../vendor/autoload.php';
+            $sid    = "ACbd8d939516cbd568851aad8dabe03eb9";
+            $token  = "ef2fca51d4eb54b9f1cd40004893b38d";
+            $twilio = new Client($sid, $token);
+
+            $message = $twilio->messages
+                ->create(
+                    "whatsapp:+5217225273757", // to 
+                    array(
+                        "from" => "whatsapp:+14155238886",
+                        "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje Z"
+                    )
+                );
+        } else if ($data[0] >= 1.5 && $data[0] < 2.9) {
+
+            require_once '../vendor/autoload.php';
+            $sid    = "ACbd8d939516cbd568851aad8dabe03eb9";
+            $token  = "ef2fca51d4eb54b9f1cd40004893b38d";
+            $twilio = new Client($sid, $token);
+
+            $message = $twilio->messages
+                ->create(
+                    "whatsapp:+5217225273757", // to 
+                    array(
+                        "from" => "whatsapp:+14155238886",
+                        "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje Z"
+                    )
+                );
+        } else {
+            echo 'ok';
+        }
+
         return response()->json(compact('labels', 'data'));
     }
     public function dataalertx()
     {
-        
+
         $graficas = grafica_ejes::latest()->take(1)->get()->sortBy('id');
         $labels = $graficas->pluck('id');
         $data = $graficas->pluck('ejex');
-        
+
         if ($data[0] >= 2.9) {
 
-        require_once '../vendor/autoload.php';
-        $sid    = "ACbd8d939516cbd568851aad8dabe03eb9"; 
-        $token  = "ef2fca51d4eb54b9f1cd40004893b38d"; 
-        $twilio = new Client($sid, $token); 
-         
-        $message = $twilio->messages 
-                          ->create("whatsapp:+5217225273757", // to 
-                                   array( 
-                                       "from" => "whatsapp:+14155238886",       
-                                       "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje x" 
-                                   ) 
-                          );
+            require_once '../vendor/autoload.php';
+            $sid    = "ACbd8d939516cbd568851aad8dabe03eb9";
+            $token  = "ef2fca51d4eb54b9f1cd40004893b38d";
+            $twilio = new Client($sid, $token);
+
+            $message = $twilio->messages
+                ->create(
+                    "whatsapp:+5217225273757", // to 
+                    array(
+                        "from" => "whatsapp:+14155238886",
+                        "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje x"
+                    )
+                );
         } else if ($data[0] >= 1.5 && $data[0] < 2.9) {
 
             require_once '../vendor/autoload.php';
-            $sid    = "ACbd8d939516cbd568851aad8dabe03eb9"; 
-            $token  = "ef2fca51d4eb54b9f1cd40004893b38d"; 
-            $twilio = new Client($sid, $token); 
-             
-            $message = $twilio->messages 
-                              ->create("whatsapp:+5217225273757", // to 
-                                       array( 
-                                           "from" => "whatsapp:+14155238886",       
-                                           "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje x" 
-                                       ) 
-                              );
-            }else{
-                echo'ok';
-                    }
+            $sid    = "ACbd8d939516cbd568851aad8dabe03eb9";
+            $token  = "ef2fca51d4eb54b9f1cd40004893b38d";
+            $twilio = new Client($sid, $token);
+
+            $message = $twilio->messages
+                ->create(
+                    "whatsapp:+5217225273757", // to 
+                    array(
+                        "from" => "whatsapp:+14155238886",
+                        "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje x"
+                    )
+                );
+        } else {
+            echo 'ok';
+        }
 
         return response()->json(compact('labels', 'data'));
     }
@@ -82,6 +120,41 @@ class VibracionController extends Controller
         $graficas = grafica_ejes::latest()->take(1)->get()->sortBy('id');
         $labels = $graficas->pluck('id');
         $data = $graficas->pluck('ejey');
+        
+        if ($data[0] >= 2.9) {
+
+            require_once '../vendor/autoload.php';
+            $sid    = "ACbd8d939516cbd568851aad8dabe03eb9";
+            $token  = "ef2fca51d4eb54b9f1cd40004893b38d";
+            $twilio = new Client($sid, $token);
+
+            $message = $twilio->messages
+                ->create(
+                    "whatsapp:+5217225273757", // to 
+                    array(
+                        "from" => "whatsapp:+14155238886",
+                        "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje Y"
+                    )
+                );
+        } else if ($data[0] >= 1.5 && $data[0] < 2.9) {
+
+            require_once '../vendor/autoload.php';
+            $sid    = "ACbd8d939516cbd568851aad8dabe03eb9";
+            $token  = "ef2fca51d4eb54b9f1cd40004893b38d";
+            $twilio = new Client($sid, $token);
+
+            $message = $twilio->messages
+                ->create(
+                    "whatsapp:+5217225273757", // to 
+                    array(
+                        "from" => "whatsapp:+14155238886",
+                        "body" => "Alerta Crítica! , Revisa Tu módulo 'vibracion' en Eje Y"
+                    )
+                );
+        } else {
+            echo 'ok';
+        }
+
         return response()->json(compact('labels', 'data'));
     }
 }
