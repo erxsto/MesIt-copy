45ff9074bc6bd9a9a869f7a8dd6c31e59e2fa582
fbianx <al221911909@gmail.com>
2021-10-27 08:53:17 -0500

    dateformat working

--- app/Http/Controllers/CorreoController.php
+++ app/Http/Controllers/CorreoController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 use App\Models\alertas;
 use Illuminate\Http\Request;
 
+use Carbon\Carbon;
 class CorreoController extends Controller
 {
+    public function getFromDateAttribute($value) {
+        return \Carbon\Carbon::parse($value)->format('d-m-Y');
+    }
     public function correo_alerta(Request $request){
 
-        $fi = $request->fecha_ini . ' 00:00:00';
-        $ff = $request->fecha_fin . ' 23:59:59';
-        $alertas = alertas::whereBetween('created_at', [$fi, $ff])->limit(5)->get();
-
+        $fi = $request->fecha_ini;
+        $ff = $request->fecha_fin;
+        $fii= Carbon::parse($fi)->format('d/m/Y');
+        $fff= Carbon::parse($ff)->format('d/m/Y');
+        $alertas = \DB::select("SELECT * FROM dbo.alertas where created_at between convert(datetime,'$fii') and convert(datetime,'$fff')");
+     
         return view('content.correo_alerta')
             ->with(['alertas' => $alertas]);
             
