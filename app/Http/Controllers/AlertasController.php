bc510c512ca1c1b3f1889b4bd712f4c01c27c19d
fbianx <al221911909@gmail.com>
2021-11-04 11:51:45 -0600

    alertas consulta

--- app/Http/Controllers/AlertasController.php
+++ app/Http/Controllers/AlertasController.php
@@ -8,17 +8,23 @@ use Carbon\Carbon;
 class AlertasController extends Controller
 {
     public function alertas(Request $request){
+        $tabla = $request->get('slcm');
 
         $fi = $request->fecha_ini;
         $ff = $request->fecha_fin;
+                
         $fii= Carbon::parse($fi)->format('d/m/Y');
+        
         $fff= Carbon::parse($ff)->format('d/m/Y');
-        $alertas = DB::select("SELECT * FROM dbo.alertas where created_at between convert(datetime,'$fii') and convert(datetime,'$fff')");
-     
-        return view('content.alertas')
-            ->with(['alertas' => $alertas]);
-            
+        
+        $alertas = DB::select("SELECT TOP 10 * FROM dbo.alertas where tabla = '$tabla' and created_at between convert(datetime,'$fii') and convert(datetime,'$fff') ");
+
+
+        return view('content.alertas')->with(compact('alertas','tabla'));
     }
+
+            
+    
 
     public function alertashow(){
         $alertas = DB::select("SELECT TOP 4 * FROM dbo.alertas ORDER BY ID DESC");
--- resources/views/content/alertas.blade.php
+++ resources/views/content/alertas.blade.php
@@ -5,18 +5,21 @@
 
 
   <center><br><h5>Consulta de Historial de Alertas</h5><br><br></center>
-   <select name="" id="" class="form-control"> 
-
-    <option value="">-- Selecciona el módulo --</option>
-    <option value="temperatura">Temperatura</option>
-    <option value="vibracion">Vibración</option>
-
-    </select>
-
+ 
 <br>
 <br>
   
 <form class="form-inline" method="get" action="{{route('alertas')}}">
+
+<select name="slcm" id="slcm" class="form-control"> 
+
+<option value="">-- Selecciona el módulo --</option>
+<option value="temperatura">Temperatura</option>
+<option value="vibracion">Vibración</option>
+
+</select>
+
+
 <label>Fecha Desde:</label>
   <input type="date" class="form-control" placeholder="Start" id="fecha_ini" name="fecha_ini" />
   <label>Hasta</label>
@@ -24,27 +27,72 @@
   
   <input type="date" class="form-control" placeholder="End" id="fecha_fin" name="fecha_fin" /><br>
   <button class="btn btn-primary" name="search"><span class="glyphicon glyphicon-search">Consultar</span></button>
+
 </form><br>
+@if(isset($tabla))
+@if($tabla=='temperatura')
+
 <div class="table-responsive">
   <table class="table table-bordered" id="order-listing" width="100%" cellspacing="0">
     <thead class="thead-dark">
       <tr>
         <th scope="col">Id</th>
+        <th scope="col">Tabla</th>
+        <th scope="col">Descripcion</th>
         <th scope="col">Temperatura</th>
         <th scope="col">Fecha de creación</th>
       </tr>
     </thead>
     <tbody>
+      
       @foreach ($alertas as $alerta)
       <tr>
+
         <td>{{$alerta->id}}</td>
+        <td>{{$alerta->tabla}}</td>
+        <td>{{$alerta->descripcion}}</td>
         <td>{{ number_format($alerta->valor,1) }}</td>
         <td>{{$alerta->created_at}}</td>
       </tr>
+      
       @endforeach
     </tbody>
   </table>
 </div>
+
+@elseif($tabla=='vibracion')
+
+  <div class="table-responsive">
+  <table class="table table-bordered" id="order-listing" width="100%" cellspacing="0">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Tabla</th>
+        <th scope="col">Descripcion</th>
+        <th scope="col">Valor</th>
+        <th scope="col">Fecha de creación</th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach ($alertas as $alerta)
+      <tr>
+      
+        <td>{{$alerta->id}}</td>
+        <td>{{$alerta->tabla}}</td>
+        <td>{{$alerta->descripcion}}</td>
+        <td>{{ number_format($alerta->valor,1) }}</td>
+        <td>{{$alerta->created_at}}</td>
+      </tr>
+      
+    @endforeach
+  @endif
+  @else
+  @endif
+    </tbody>
+  </table>
+</div>
+
+
 <div class="alert alert-dark alert-sm" role="alert">
   <h4 class="alert-heading">Descarga el reporte.</h4><br>
   <form class="form-inline" method="get" action="{{route('descargarPDFt')}}">
